bcf1ae90f9fbde5aecb96efa90c86cb2cab9ab70
Samson Nwokike <nwokikesamson@gmail.com>
2024-07-17 04:23:56 +0100

    added the server file

--- cmd/ecomm-api/main.go
+++ cmd/ecomm-api/main.go
@@ -4,13 +4,19 @@ import (
 	"log"
 
 	"github.com/sir-radar/go-ecom/db"
+	"github.com/sir-radar/go-ecom/ecomm-api/server"
+	"github.com/sir-radar/go-ecom/ecomm-api/storer"
 )
 
-func main(){
+func main() {
 	db, err := db.NewDatabase()
-	if err != nil{
+	if err != nil {
 		log.Fatalf("error opening database: %v", err)
 	}
 	defer db.Close()
 	log.Println("successfully connected to database")
+
+	st := storer.NewMySQLStorer(db.GetDB())
+	_ = server.NewServer(st)
+	// hdl := handler.NewHandler(srv)
 }
